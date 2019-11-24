6c4aeb50064bff6b90b93d2afd2f88b133c3848d
tk6996 <watcharinkanha@gmail.com>
2019-11-24 14:15:23 +0700

    fifth commit

--- src/com/epam/Control.java
+++ src/com/epam/Control.java
@@ -6,33 +6,30 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.comm.CommPortIdentifier;
+import javax.comm.PortInUseException;
 import javax.comm.SerialPort;
+import javax.comm.UnsupportedCommOperationException;
 
 public class Control {
-    
+
     public CommPortIdentifier portId;
-	public InputStream inputStream;
-	public OutputStream outputStream;
+    public InputStream inputStream;
+    public OutputStream outputStream;
     public SerialPort serialPort;
-    
-    public Control(CommPortIdentifier pid)
+    public BufferedInputStream bw;
+
+    public Control(CommPortIdentifier pid) throws PortInUseException, IOException, UnsupportedCommOperationException
     {
-        try {
 			portId = pid;
 			serialPort = (SerialPort) portId.open("Control", 1000);
             inputStream = serialPort.getInputStream();
             outputStream = serialPort.getOutputStream();
+            bw = new BufferedInputStream(inputStream);
 			serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                    SerialPort.PARITY_NONE);
     }
     public int waitingStart() throws IOException
     {
-       // while (inputStream.available() == 0)
-       // {}
-        BufferedInputStream bw = new BufferedInputStream(inputStream);
         int ch =  bw.read();
         System.out.println((char)ch);
         return ch;
